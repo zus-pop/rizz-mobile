b8e6931313b9fbbd02df99b09512ed7213b1ae5e
zus-pop <hoanganhtu3979@gmail.com>
2025-07-11 22:44:46 +0700

    remove .env file

---
+++ app/phone-register.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const PhoneRegister = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>PhoneRegister</Text>
+    </View>
+  );
+};
+
+export default PhoneRegister;
